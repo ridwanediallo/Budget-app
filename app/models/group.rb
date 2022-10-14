f0b104557df6ab2f0d5c212bef4e5c63cfaa1e14
ridwan <ridwanediallo99@gmail.com>
2022-10-14 18:42:22 +0000

    Update relationships in Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user, :optional => true
-  has_many :entities , through: :group_entities
+  has_many :entities
+  mount_uploader :icon, IconUploader
+
 end
